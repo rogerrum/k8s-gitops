4437699a9ea489e3cf11cf1f708391973f640a53
Roger Rumao <rumaor1@nationwide.com>
2021-03-17 16:53:37 -0500

    add statping secret

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -214,6 +214,7 @@ loadSecretsToVault() {
 
   secretVault "infrastructure/oauth2-proxy/oauth2-proxy-secret.txt"
   secretVault "infrastructure/postgresql/postgresql-secret.txt"
+  secretVault "main/monitoring/statping/statping-secret.txt"
 
   #vault kv put secrets/flux-system/discord-webhook address="$DISCORD_FLUX_WEBHOOK_URL"
   #kvault "main/kube-system/kured/kured-helm-values.txt"
@@ -245,7 +246,7 @@ fi
 
 #loadSecretsToVault
 
-secretVault "infrastructure/postgresql/postgresql-secret.txt"
+secretVault "main/monitoring/statping/statping-secret.txt"
 
 
 kill $VAULT_FWD_PID
