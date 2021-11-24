4da3ca116eea3bc9c70f1ad4c201be421ea8ff9e
Roger Rumao <rumaor1@nationwide.com>
2021-11-24 16:25:06 -0600

    fix

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -193,6 +193,7 @@ EOF
 
   # Tell Vault how to communicate with the Kubernetes cluster
   vault write auth/kubernetes/config \
+    issuer="https://kubernetes.default.svc.cluster.local" \
     token_reviewer_jwt="$SA_JWT_TOKEN" \
     kubernetes_host="$K8S_HOST" \
     kubernetes_ca_cert="$SA_CA_CRT"
@@ -204,6 +205,33 @@ EOF
     policies=vault-secrets-operator \
     ttl=24h
 }
+
+fixVaultSecretsOperator() {
+  message "Fixing vault for vault-secrets-operator"
+
+  export VAULT_SECRETS_OPERATOR_NAMESPACE=$(kubectl -n kube-system get sa vault-secrets-operator -o jsonpath="{.metadata.namespace}")
+  export VAULT_SECRET_NAME=$(kubectl -n kube-system get sa vault-secrets-operator -o jsonpath="{.secrets[*]['name']}")
+  export SA_JWT_TOKEN=$(
+    kubectl -n kube-system get secret $VAULT_SECRET_NAME -o jsonpath="{.data.token}" | base64 --decode
+    echo
+  )
+  export SA_CA_CRT=$(
+    kubectl -n kube-system get secret $VAULT_SECRET_NAME -o jsonpath="{.data['ca\.crt']}" | base64 --decode
+    echo
+  )
+  export K8S_HOST=$(kubectl -n kube-system config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+
+  # Verify the environment variables
+  # env | grep -E 'VAULT_SECRETS_OPERATOR_NAMESPACE|VAULT_SECRET_NAME|SA_JWT_TOKEN|SA_CA_CRT|K8S_HOST'
+
+  # Tell Vault how to communicate with the Kubernetes cluster
+  vault write auth/kubernetes/config \
+    issuer="https://kubernetes.default.svc.cluster.local" \
+    token_reviewer_jwt="$SA_JWT_TOKEN" \
+    kubernetes_host="$K8S_HOST" \
+    kubernetes_ca_cert="$SA_CA_CRT"
+}
+
 
 loadSecretsToVault() {
   message "writing secrets to vault"
@@ -262,6 +290,8 @@ fi
 #loadSecretsToVault
 #helmVault "main/logs/loki/loki-helm-values.txt"
 
-argoHelmValueVault "main/homelab/emqx/emqx-helm-values.txt"
+#argoHelmValueVault "main/homelab/emqx/emqx-helm-values.txt"
+
+fixVaultSecretsOperator
 
 kill $VAULT_FWD_PID
