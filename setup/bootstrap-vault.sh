250466cf88b12897ddb11c8b74a3dd8b4fc792c5
Roger Rumao <rumaor1@nationwide.com>
2021-11-27 01:42:31 -0600

    add frigate secret

--- setup/bootstrap-vault.sh
+++ setup/bootstrap-vault.sh
@@ -271,6 +271,7 @@ loadSecretsToVault() {
   secretVault "main/homelab/mealie/mealie-secret.txt"
   secretVault "main/homelab/traccar/traccar-secret.txt"
   secretVault "main/monitoring/unifi-poller/unifi-poller-secret.txt"
+  secretVault "main/homelab/frigate/frigate-secret.txt"
 
   vault kv put secrets/argocd/argocd-discord-webhook discord-webhook="$DISCORD_ARGO_WEBHOOK_URL"
 
@@ -290,6 +291,6 @@ if [ $FIRST_RUN == 0 ]; then
 fi
 
 #loadSecretsToVault
-secretVault "main/monitoring/unifi-poller/unifi-poller-secret.txt"
+secretVault "main/homelab/frigate/frigate-secret.txt"
 
 kill $VAULT_FWD_PID
